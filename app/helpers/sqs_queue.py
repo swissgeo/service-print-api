d01d4b207d3e50d21ad8e9e60731484d3bc26243
Tobias Reber <tobias.reber@swisstopo.ch>
2026-04-20 15:33:33 +0200

    GPS 648: No exception handling when testing the length of the queue

--- app/helpers/sqs_queue.py
+++ app/helpers/sqs_queue.py
@@ -68,21 +68,16 @@ def is_queue_overloaded() -> bool:
     On any SQS error, fails open (returns False) so the caller can proceed.
     """
     sqs = get_sqs_client()
-    try:
-        queue_url = sqs.get_queue_url(QueueName=SQS_QUEUE_NAME)["QueueUrl"]
-        response = sqs.get_queue_attributes(
-            QueueUrl=queue_url,
-            AttributeNames=["ApproximateNumberOfMessages"],
-        )
-        length = int(response["Attributes"]["ApproximateNumberOfMessages"])
-        logger.debug(
-            "SQS queue %s has %d messages (max: %d)", SQS_QUEUE_NAME, length, SQS_QUEUE_MAX_LENGTH
-        )
-    except ConnectTimeoutError, ReadTimeoutError, ClientError:
-        logger.exception("Could not check queue length for %s, failing open", SQS_QUEUE_NAME)
-        return False
-    else:
-        return length > SQS_QUEUE_MAX_LENGTH
+    queue_url = sqs.get_queue_url(QueueName=SQS_QUEUE_NAME)["QueueUrl"]
+    response = sqs.get_queue_attributes(
+        QueueUrl=queue_url,
+        AttributeNames=["ApproximateNumberOfMessages"],
+    )
+    length = int(response["Attributes"]["ApproximateNumberOfMessages"])
+    logger.debug(
+        "SQS queue %s has %d messages (max: %d)", SQS_QUEUE_NAME, length, SQS_QUEUE_MAX_LENGTH
+    )
+    return length > SQS_QUEUE_MAX_LENGTH
 
 
 def send_to_queue(message: dict[str, Any]) -> None:
