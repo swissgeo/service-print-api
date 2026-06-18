d73900394e068bb964826d4f2c6df6c21e9d7729
Tobias Reber <tobias.reber@swisstopo.ch>
2026-06-18 11:02:16 +0200

    GPS 789: change `print_lang` to be a mandatory field in the POST payload

--- app/schemas/jobs.py
+++ app/schemas/jobs.py
@@ -40,8 +40,8 @@ class PrintJobPayload(BaseModel):
     print_grid: bool | None = Field(
         default=None, description="Whether to overlay a grid on the map"
     )
-    print_lang: Literal["de", "fr", "it", "en", "rm"] | None = Field(
-        default=None, description="Language for the print output"
+    print_lang: Literal["de", "fr", "it", "en", "rm"] = Field(
+        description="Language for the print output"
     )
 
 
